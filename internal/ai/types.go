bcbdcb66c7674f60e56a7cd1ceac417fa437971d
rdmm123 <robedanilo2001@gmail.com>
2025-09-28 00:07:12 -0400

    remove unused types

--- internal/ai/types.go
+++ internal/ai/types.go
@@ -1,23 +1,6 @@
 package ai
 
-import (
-	"encoding/json"
-	"fmt"
-
-	"google.golang.org/genai"
-)
-
-type GenerationConfig struct {
-	Model       string
-	Temperature float32
-}
-
-
 type AttachmentFile []byte
-
-// func (a AttachmentFile) MarshalJSON() ([]byte, error) {
-// 	return []byte(fmt.Sprintf("File of size %d kB", len(a) / 1024)), nil
-// }
 
 type Attachment struct {
 	URI string
@@ -44,30 +27,6 @@ type MessageUser struct {
 type MessageHousehold struct {
 	ID int32
 	Name string
-}
-
-// this is just to override the json marshalling of files
-type CustomContent struct {
-	Parts []*CustomPart `json:"parts,omitempty"`
-	genai.Content
-}
-
-type CustomPart struct {
-	InlineData *CustomBlob `json:"inlineData,omitempty"`
-	genai.Part
-}
-
-type CustomBlob genai.Blob
-
-func (b *CustomBlob) MarshalJson() ([]byte, error) {
-	type Alias CustomBlob
-	return json.Marshal(&struct {
-		Data string `json:"data,omitempty"`
-		*Alias
-	}{
-		Data: fmt.Sprintf("File of size %d kB", len(b.Data) / 1024),
-		Alias:    (*Alias)(b),
-	})
 }
 
 type UsageStats struct {
